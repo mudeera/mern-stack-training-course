9b0fa3f64041b22dc43d20588696253a1462d4e4
mudeera <mudeerayousuf584@gmail.com>
2023-03-25 16:58:53 +0500

    array concat file

---
+++ js/arrconcat.js
@@ -0,0 +1,4 @@
+const myarray = [5];
+const myarray1 = [15];
+result = myarray.concat(myarray1);
+console.log(result);
---
+++ js/pop.js
@@ -0,0 +1,3 @@
+const myarray = [1, 2, 3, 4, 5];
+myarray.pop();
+console.log(myarray);
